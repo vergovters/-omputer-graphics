379c1162b3067bf25445a768e9d1a782f8884f39
illia <fialkivskiji@gmail.com>
2024-03-25 22:34:32 +0200

    teture lab4

--- lab3/task2.js
+++ lab3/task2.js
@@ -3,12 +3,7 @@ import * as THREE from 'three';
 export const task2 = () => {
   
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75, 
-  window.innerWidth / window.innerHeight, 
-  0.1, 
-  1000 
-);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
